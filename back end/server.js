2548f2ead4cda5250f9b9fa4089184a4534daf4d
zennon-sml <zennonsampaioml@gmail.com>
2025-06-30 10:38:34 -0300

    MVC estructure

--- back end/server.js
+++ back end/server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+todoRoutes = require("./routes/todo")
+app.use("/todo", todoRoutes)
 
 app.listen(port, () => {
     console.log(`App listening on port: ${port}`)
